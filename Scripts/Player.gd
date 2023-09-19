71980f9544071d048e99d69903451bdfa7786523
justlimeguy <142280135+justlimeguy@users.noreply.github.com>
2023-09-19 10:38:34 +0530

    raycast cod reduction

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -98,8 +98,9 @@ func _on_animated_sprite_2d_animation_finished():
 func playerState():
 	pass
 func shoot_ray():
+
 	if ray_cast_2d.is_colliding() == false:
 		ray_cast_2d.target_position.x += 25
 	if ray_cast_2d.is_colliding() == true:
 		ray_cast_2d.target_position.x = 0
-		ray_cast_2d.target_position.x += 25
+		
