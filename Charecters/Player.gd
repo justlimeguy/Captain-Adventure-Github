1e902414ec29756e2473909881c81b0df027bfff
justlimeguy <142280135+justlimeguy@users.noreply.github.com>
2023-10-22 08:57:15 +0530

    fnnnnnnnng

--- Charecters/Player.gd
+++ Charecters/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
-@onready var particles: AnimatedSprite2D = $Particles
+
 @onready var player_animatios: AnimatedSprite2D = $PlayerAnimatios
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,6 +19,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
-	
+	animations_and_particles()
 func animations_and_particles():
-	pass
+	if velocity.x != 0 && is_on_floor():
+		player_animatios.play("run")
+	if velocity.x == 0 && velocity.y == 0:
+		player_animatios.play("idle")
+	if velocity.y > 0 && !is_on_floor():
+		player_animatios.play("fall")
+	if velocity.y < 0 && Input.is_action_just_pressed("jump"):
+		player_animatios.play("jump")
+	if Input.is_action_just_pressed("left"):
+		player_animatios.flip_h = true
+	if Input.is_action_just_pressed("right"):
+		player_animatios.flip_h = false
