83ab08228c8a907b0dc376acd377aaf964ce6928
justlimeguy <142280135+justlimeguy@users.noreply.github.com>
2023-10-24 09:15:35 +0530

    ausid

--- Charecters/Player.gd
+++ Charecters/Player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
-@onready var player_animatios: AnimatedSprite2D = $PlayerAnimatios
+@onready var player_animatios: AnimationPlayer = $AnimationPlayer
+@onready var sprite = $PlayerAnimatios
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var player_flipped : bool
@@ -23,19 +24,20 @@ func _physics_process(delta: float) -> void:
 	maual_flip()
 func animations_and_particles():
 	if velocity.x != 0 && is_on_floor():
-		player_animatios.play("run")
+		player_animatios.play("Run")
+		
 	if velocity.x == 0 && velocity.y == 0:
-		player_animatios.play("idle")
+		player_animatios.play("Idle")
 	if velocity.y > 0 && !is_on_floor():
-		player_animatios.play("fall")
+		player_animatios.play("Fall")
 	if velocity.y < 0 && Input.is_action_just_pressed("jump"):
-		player_animatios.play("jump")
+		player_animatios.play("Jump")
 	if Input.is_action_just_pressed("left"):
 		player_flipped = true
 	if Input.is_action_just_pressed("right"):
 		player_flipped = false
 func maual_flip():
 	if player_flipped == true:
-		player_animatios.flip_h = true
+		sprite.flip_h = true
 	else:
-		player_animatios.flip_h = false
+		sprite.flip_h = false
